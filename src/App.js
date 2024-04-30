baa7b6edf09b363284a661eae6ddecdd8e389297
trail_ <chantichintu5555@gmail.com>
2024-04-30 17:50:32 +0530

    project commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my portfolio.
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          rel="noopener noreferrer"
+        >
+          Learn learning
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
